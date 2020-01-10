e0e7a91c8c6958585cc315a6f68e4c0b1dfab362
unknown <dustin.guy.jackson@gmail.com>
2020-01-10 12:06:18 -0800

    tied all input elements to JS, and got my local storage to store anything added into the text area

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const timeSlot = document.querySelector(".timeslot");
 const hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
 
-
 for (i = 0; i < hours.length; i++) {
     let div = document.createElement("div");
     let save = document.createElement("button");
@@ -10,19 +9,68 @@ for (i = 0; i < hours.length; i++) {
     div.innerText = hours[i];
     div.setAttribute("class", "hour");
     save.innerText = "Save";
+    save.setAttribute("class", "button");
     save.setAttribute("value", hours[i]);
     div.appendChild(input)
     div.appendChild(save);
-    let fetch = JSON.parse(localStorage.getItem(hours[i]));
-    if (fetch === null) fetch = [];
-    console.log(fetch)
+    let hour = JSON.parse(localStorage.getItem(hours[i]));
+    if (hour === null) hour = [];
+    localStorage.setItem(hours[i], JSON.stringify(hour));
+    input.innerText = JSON.parse(localStorage.getItem(hours[i]));
+    // let toDo = localStorage.getItem(hours[i]);
+    // console.log(toDo)
+}
+///event listeners on all text areas
+let text = document.querySelectorAll("textarea");
+text[0].addEventListener('input', update0);
+text[1].addEventListener('input', update1);
+text[2].addEventListener('input', update2);
+text[3].addEventListener('input', update3);
+text[4].addEventListener('input', update4);
+text[5].addEventListener('input', update5);
+text[6].addEventListener('input', update6);
+text[7].addEventListener('input', update7);
+text[8].addEventListener('input', update8);
+
+///updates the textareas, adding text value to them
+function update0 (e) {
+    text[0].textContent = e.target.value;
+}
+function update1 (e) {
+    text[1].textContent = e.target.value;
+}
+function update2 (e) {
+    text[2].textContent = e.target.value;
+}
+function update3 (e) {
+    text[3].textContent = e.target.value;
+}
+function update4 (e) {
+    text[4].textContent = e.target.value;
+}
+function update5 (e) {
+    text[5].textContent = e.target.value;
+}
+function update6 (e) {
+    text[6].textContent = e.target.value;
+}
+function update7 (e) {
+    text[7].textContent = e.target.value;
+}
+function update8 (e) {
+    text[8].textContent = e.target.value;
 }
 
-// let hourList = document.querySelectorAll(".hour");
-// console.log(hourList)
+let buttons = document.querySelectorAll(".button");
+document.addEventListener('click', function (e) {
+    
+    for (i = 0; i < buttons.length; i++) {
+    if (e.target === buttons[i]) {
+            var entry = text[i].textContent;
+            localStorage.setItem(buttons[i].value, JSON.stringify(entry));
+        }   
+    }
+    
+})
 
-// document.addEventListener('click', function(e) {
-//     let a = e.target.value
-//         console.log(a);
 
-// })
